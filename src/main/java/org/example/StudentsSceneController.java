621ee060b72b6eb433ed56b2010fa7222a053ffe
John Smith <you@inf.unideb.hu>
2024-10-01 11:22:46 +0200

    MVC components ok

--- src/main/java/org/example/StudentsSceneController.java
+++ src/main/java/org/example/StudentsSceneController.java
@@ -3,10 +3,28 @@ package org.example;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import org.example.model.Model;
 
 public class StudentsSceneController {
+
+    private Model model = new Model();
+
     @FXML
     private Label colorLabel;
+
+    @FXML
+    private Label creditsLabel;
+
+    @FXML
+    private Label dateOfBirthLabel;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    void loadData(ActionEvent event) {
+
+    }
 
     @FXML
     void changeColor(ActionEvent event) {
